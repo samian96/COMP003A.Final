ff82ddeb793ef96115dd1764c0df6201cbcd86fb
samian <sam.iannazzo@yahoo.com>
2026-02-26 22:57:31 -0800

    Add project files.

---
+++ COMP003A.Final/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
