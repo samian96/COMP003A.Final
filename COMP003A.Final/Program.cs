bc075b1ad33d3822f470bd19e4ddcb7eb94181e1
samian <sam.iannazzo@yahoo.com>
2026-02-26 23:10:28 -0800

    Created MenuDisplay Method with consolewritelines / added display to main method

--- COMP003A.Final/Program.cs
+++ COMP003A.Final/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool running = true;
+
+            MenuDisplay();
+
+            while (running)
+            {
+                Console.ReadLine();
+            }
+        }
+        static void MenuDisplay()
+        {
+            Console.WriteLine("Character Creation Menu:");
+            Console.Write("1. Create Character: \n 2. View All Characters: \n 3: View Character Stats: \n Remove Character: \n Exit Program.");
         }
     }
 }
