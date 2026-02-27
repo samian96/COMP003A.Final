0a4dbc7af2a883eacd8d06fbf1f7ff42a321e33a
samian <sam.iannazzo@yahoo.com>
2026-02-26 23:25:46 -0800

    created class Character Creation/ added int and string to it

---
+++ COMP003A.Final/CharacterCreation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Final
+{
+    internal class CharacterCreation
+    {
+        string Name;
+
+        string Class;
+
+        string Species;
+
+        int Health;
+
+        int MP;
+
+        int Level;
+    }
+}
