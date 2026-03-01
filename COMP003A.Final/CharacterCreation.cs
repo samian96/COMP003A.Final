f3b0c7d6a76dd319b80b239ae113c889552f75ba
samian <sam.iannazzo@yahoo.com>
2026-03-01 14:37:05 -0800

    adding more customizable fields for character creation

--- COMP003A.Final/CharacterCreation.cs
+++ COMP003A.Final/CharacterCreation.cs
@@ -6,6 +6,7 @@ namespace COMP003A.Final
 {
     public class CharacterCreation
     {
+        // main class customization
         public string Name;
 
         public string ClassName;
@@ -17,7 +18,53 @@ namespace COMP003A.Final
         public int MP;
 
         public int Level = 1;
-    
+
+        // deeper customization of character
+
+        public string Age;
+
+        public string Gender;
+
+        public string HairColor;
+
+        public string EyeColor;
+
+        public bool IsAlive = true;
+
+        public string Weapon;
+
+        public string Armor;
+
+        // sub division of abilities for combat health and ability 
+
+        public int MaxHealth;
+
+        public int CurrentHealth;
+
+        public int MaxMP;
+
+        public int CurrentMP;
+
+        public int Strength;
+
+        public int Dexterity;
+
+        public int Defense;
+
+        public int Intelligence;
+
+        public int Wisdom;
+
+        public int Speed;
+
+        // Progress stats for battle if implemented
+
+        public int Experience;
+
+        public int Gold;
+
+
+        // this section is to display everything in a list of the character created
         public void DisplayCharacter()
         {
             Console.WriteLine();
@@ -33,6 +80,44 @@ namespace COMP003A.Final
             Console.WriteLine("MP: " + MP);
 
             Console.WriteLine("Level: " + Level);
+
+            Console.WriteLine("Age: " + Age);
+
+            Console.WriteLine("Gender: " + Gender);
+
+            Console.WriteLine("Hair Color: " + HairColor);
+
+            Console.WriteLine("Eye Color: " + EyeColor);
+
+            Console.WriteLine("Status: " + IsAlive);
+
+            Console.WriteLine("Weapon: " + Weapon);
+
+            Console.WriteLine("Armor: " + Armor);
+
+            Console.WriteLine("MaxHealth: " + MaxHealth);
+
+            Console.WriteLine("CurrentHealth: " + CurrentHealth);
+
+            Console.WriteLine("MaxMP: " + MaxMP);
+
+            Console.WriteLine("CurrentMP: " + CurrentMP);
+
+            Console.WriteLine("Strength: " + Strength);
+
+            Console.WriteLine("Dexterity: " + Dexterity);
+
+            Console.WriteLine("Defense: " + Defense);
+
+            Console.WriteLine("Intelligence: " + Intelligence);
+
+            Console.WriteLine("Wisdom: " + Wisdom);
+
+            Console.WriteLine("Speed: " + Speed);
+
+            Console.WriteLine("Experience: " + Experience);
+
+            Console.WriteLine("Gold: " + Gold);
 
             Console.WriteLine();
         }
